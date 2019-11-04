9b1a0531aab0e12fc3a3296f9d5a10ea693a05e2
androidlutz <55858485+androidlutz@users.noreply.github.com>
2019-11-04 15:30:50 -0800

    Remake of "guess my number"

---
+++ cs160-ch5 exercises/src/cs160_Ch5_conditionalsAndLogic/GuessMyNumberAgain.java
@@ -0,0 +1,55 @@
+
+
+
+package cs160_Ch5_conditionalsAndLogic;
+
+
+import java.util.Random;
+import java.util.Scanner;
+public class GuessMyNumberAgain {
+	static Scanner enter = new Scanner(System.in);
+
+	
+	
+	public static void main(String[] args) {
+		 Random random = new Random();
+	      int answer = random.nextInt(100 + 1);
+	      guessNumber(answer);
+	}    
+	
+	public static void guessNumber(int answer) {
+		
+		//Scanner scan = new Scanner(System.in);
+		 System.out.println("I'm thinking of a number between 1 and 100, (including both), Can you guess what it is?");
+	        System.out.println("Type a number: ");
+	        
+	        int yourNumber = enter.nextInt();
+	        
+	        
+	        if(yourNumber == answer) {
+	        	System.out.println("That is correct");
+	        	
+	        }
+	        //compare the two numbers
+	        if(yourNumber < answer) {
+	        	System.out.println("Too low, guess again");
+		     //   int newguessNumber = enter.nextInt();
+
+	        }
+	        if 
+	        	(yourNumber > answer) {
+	        	System.out.println("Too high, guess again");
+		      //  int newguessNumber = enter.nextInt();
+
+	        }
+	        else {
+	        	guessNumber(answer);
+	        	
+	        }
+	        
+	
+	
+	
+}	
+	
+}
